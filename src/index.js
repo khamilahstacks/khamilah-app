31f1ba9cc032bc4ca0207075e2dcd892a91c1201
khamilahstacks <kayanna87829@gmail.com>
2022-11-16 23:19:44 -0800

    adjustment to the forcast function

--- src/index.js
+++ src/index.js
@@ -54,7 +54,51 @@ form.addEventListener("submit", search);
 
 function displayForcast (){
 let forcast= document.querySelector("#forcast");
-forcast.innerHTML="forcast";
+
+
+let forcastHTML=`<div class="text-center" id="forcast">
+            <div class="card-group">
+                <div class="card" style="width: 18rem">
+                    <h3 class="card-title">Monday</h3>
+                    <h4 class="card-title"></h4>
+                    <p class="weather-size">🌥</p>
+                    <div class="card-body">
+                        <h4>82°🌞 / 67°🌙</h4>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                    <h3 class="card-title">Tuesday</h3>
+                    <h4 class="card-title"></h4>
+                    <div class="card-body">
+                        <p class="weather-size">☀️</p>
+                        <h4>81°🌞 / 66°🌙</h4>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                    <h3 class="card-title">Wednesday</h3>
+                    <h4 class="card-title"></h4>
+                    <div class="card-body">
+                        <p class="weather-size">🌤</p>
+                        <h4>79°🌞 / 62°🌙</h4>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                    <h3 class="card-title">Thursday</h3>
+                    <h4 class="card-title"></h4>
+                    <div class="card-body">
+                        <p class="weather-size">☀️</p>
+                        <h4>81°🌞 / 62°🌙</h4>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem">
+                    <h3 class="card-title">Friday</h3>
+                    <h4 class="card-title"></h4>
+                    <div class="card-body">
+                        <p class="weather-size">☀️</p>
+                        <h4>82°🌞 / 63°🌙</h4>
+                    </div>
+                </div>
+            </div>`;
 }
 
 displayForcast();
