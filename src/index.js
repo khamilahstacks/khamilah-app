3b1fec107fcf7c56679a0ab2fe84bd85d4543e7d
khamilahstacks <kayanna87829@gmail.com>
2022-11-23 00:05:49 -0800

    adjusting for the icon placement in the java

--- src/index.js
+++ src/index.js
@@ -63,11 +63,11 @@ function displayForecast(responsed) {
     
     ` <div class="container">
         <div class="text-center"${forecastDay} id="forecast">
-            <div class="card-group"  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png">
+            <div class="card-group"<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png">
                 <div class="card" style="width: 18rem">
                     <h3 class="card-title">Monday</h3>
                     <h4 class="card-title"></h4>
-                    <p class="weather-size"></p>
+                    <p class="weather-size"><img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"</p>
                     <div class="card-body">
                         <h4>${Math.round(forecastDay.temp.max)}° / ${Math.round(forecastDay.temp.min)}°</h4>
                     </div>
